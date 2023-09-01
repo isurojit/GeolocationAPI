5af845a60239d5702355f9e57a3a2d21f9a8c244
isurojit <iamsurojit@hotmail.com>
2023-09-01 08:49:58 +0530

    Watchcoordinates and clearWatchCoordinates

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
+//GET Current Position
 const currentSucess = (pos) => {
   //   const cordinates = pos.coords;
   const { latitude, longitude, accuracy } = pos.coords;
-  console.log(latitude, longitude, accuracy);
+  console.log("LATITUDE:", latitude);
+  console.log("LONGITUDE:", longitude);
+  console.log("ACCURACY WITHIN RAGE OF:", accuracy / 1000, "KM");
 };
 
 const currentError = (err) => {
   console.log(err);
 };
 
-const options = {};
+const currentOptions = {
+  enableHighAccuracy: true, //Use GPS if Available
+  timeout: 10000, //Time to wait to stop trying for location
+  maximumAge: 0, // Do not use a cached position
+};
 
-navigator.geolocation.getCurrentPosition(currentSucess, currentError, options);
+// navigator.geolocation.getCurrentPosition(
+//   currentSucess,
+//   currentError,
+//   currentOptions
+// );
+
+//Watch Current Position
+const target = {
+  latitude: 41.351245,
+  longitude: -71.332365,
+};
+const watchSucess = (pos) => {
+  const cordinates = pos.coords;
+  if (
+    target.latitude === cordinates.latitude &&
+    target.longitude === cordinates.longitude
+  ) {
+    console.log("arrived in destination");
+    navigator.geolocation.clearWatch(id);
+  }
+  console.log(cordinates);
+};
+
+const watchError = (err) => {
+  console.log(err);
+};
+
+const watchOptions = {
+  enableHighAccuracy: true, //Use GPS if Available
+  timeout: 10000, //Time to wait to stop trying for location
+  maximumAge: 0, // Do not use a cached position
+};
+
+const id = navigator.geolocation.watchPosition(
+  watchSucess,
+  watchError,
+  watchOptions
+);
